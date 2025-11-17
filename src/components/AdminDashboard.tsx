a75b7ee04f45abd8d793652f3813961d9ff48df8
Garion2889 <domingogabrielsantiago@gmail.com>
2025-11-17 10:35:52 +0800

    Update AdminDashboard.tsx

--- src/components/AdminDashboard.tsx
+++ src/components/AdminDashboard.tsx
@@ -230,13 +230,13 @@ export function AdminDashboard({ stats }: AdminDashboardProps) {
           <CardTitle>Live Driver Tracking</CardTitle>
         </CardHeader>
         <CardContent>
-          <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "400px", width: "100%" }}>
+          <MapContainer center={[14.5995, 120.9842]} zoom={13} style={{ height: "400px", width: "100%" }}>
             <TileLayer
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
             {/* Example Marker */}
-            <Marker position={[51.505, -0.09]} icon={defaultIcon}>
+            <Marker position={[14.5995, 120.9842]} icon={defaultIcon}>
               <Popup>
                 A live driver is here.
               </Popup>
